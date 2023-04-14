10a3ae98b4a94097d7fa1faed8156ea3a99cd9d2
Punit <ojha1111pk@gmail.com>
2023-04-14 23:34:26 +0530

    adding CORS

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config({ path: './.env' });
 const app = require('./app');
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
+  console.log(`Allowed clients : ${process.env.ALLOWED_CLIENTS}`);
   console.log(`Server listening on PORT: ${PORT}`);
 });
 module.exports = { server };
